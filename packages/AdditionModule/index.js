4326eeb6823f445d5d38c1767fb64e293b117a67
Vishesh <vishesh.agrawal@sap.com>
2019-09-09 11:30:04 +0530

    Adding cube values

--- packages/AdditionModule/index.js
+++ packages/AdditionModule/index.js
@@ -5,3 +5,7 @@ module.exports.add = function(x,y){
 module.exports.addSquares = function(x,y){
     return (x*x) + (y*y);
 }
+
+module.exports.addCube = function(x,y){
+    return (x*x*x) + (y*y*y);
+}
