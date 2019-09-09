7085e833591d86f547246a7c4f259176d2105b18
Vishesh <vishesh.agrawal@sap.com>
2019-09-09 11:22:44 +0530

    Adding method to add and subtract the squares

--- packages/Calc/index.js
+++ packages/Calc/index.js
@@ -5,3 +5,8 @@ var sum = add.add(2,3);
 var diff = subtract.subtract(3,2);
  
 console.log("Sum: " + sum + " Diff: " + diff);
+
+var sum = add.addSquares(2,3);
+var diff = subtract.subtractSquares(3,2);
+ 
+console.log("SumSquares: " + sum + " DiffSquares: " + diff);
