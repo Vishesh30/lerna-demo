5bf89e7454163ff26e7cdd96e059643b7a8d9940
Vishesh <vishesh.agrawal@sap.com>
2019-09-09 11:27:35 +0530

    Adding logic for add and sub for squares

--- packages/AdditionModule/index.js
+++ packages/AdditionModule/index.js
@@ -1,3 +1,7 @@
 module.exports.add = function(x,y){
     return x + y;
 }
+
+module.exports.addSquares = function(x,y){
+    return (x*x) + (y*y);
+}
--- packages/SubtractionModule/index.js
+++ packages/SubtractionModule/index.js
@@ -1,3 +1,7 @@
 module.exports.subtract = function(x,y){
     return x - y;
 }
+
+module.exports.subtractSquares = function(x,y){
+    return (x*x) - (y*y);
+}
